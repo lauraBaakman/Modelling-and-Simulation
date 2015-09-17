88b1471a61a05459f0cfe55fc42b0e859e2f3da2
Laura Baakman <laurabaakman@gmail.com>
2015-09-17 16:20:46 +0200

    Code to compute bifurcation diagram and some bifurcation diagrams.

---
+++ Practica/Assignment 1/code/assignment_b_bifurcation_diagram.m
@@ -0,0 +1,42 @@
+clc; clear all; close all force;
+
+Ks = 0:1/20:5;
+
+X_IDX = 1; P_IDX = 2;
+
+x_0s = [0.5, 0.1576131, 0.1269868];
+p_0s = [0.5, 0.9705928, 0.9133759];
+
+n = 1000;
+
+marker.type = '.';
+marker.size = 5;
+marker.colour = 'b';
+
+for init_idx = 1:length(x_0s)
+    
+    x_0 = x_0s(init_idx);
+    p_0 = x_0s(init_idx);
+
+    maps = nan(n, 2, length(Ks));
+    
+    for idx = 1 : length(Ks)
+        K = Ks(idx);
+        maps(:, :, idx) = chirikov_map(K, x_0, p_0, n);
+    end
+    
+        figure('Visible','off')
+        scatter(reshape(repmat(Ks, n, 1), [], 1), reshape(squeeze(maps(:, X_IDX, :)), [], 1), marker.size, marker.colour, marker.type);
+        xlabel('K')
+        ylabel('{x_n}')
+        title(sprintf('x_0 = %2.3f, p_0 = %2.3f', x_0, p_0));
+        high_quality_plot('Save', sprintf('../report/img/assignment_b_dim_%d_b_x.pdf', init_idx -1), 'FontSize', 22, 'PaperWidth', 8, 'PaperHeight', 8, 'Margin', 0, 'Box', 'on');
+    
+        figure('Visible','off')
+        scatter(reshape(repmat(Ks, n, 1), [], 1), reshape(squeeze(maps(:, P_IDX, :)), [], 1), marker.size, marker.colour, marker.type);
+        xlabel('K')
+        ylabel('{p_n}')
+        title(sprintf('x_0 = %2.3f, p_0 = %2.3f', x_0, p_0));        
+        high_quality_plot('Save', sprintf('../report/img/assignment_b_dim_%d_b_p.pdf', init_idx -1), 'FontSize', 22, 'PaperWidth', 8, 'PaperHeight', 8, 'Margin', 0, 'Box', 'on');
+        
+end
