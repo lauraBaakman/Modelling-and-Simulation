4c509db2ff41fff0639eae5b43caf56ac1ce5ace
Rick van Veen <r.van.veen133@gmail.com>
2015-09-17 13:39:10 +0200

    Meer plaatjes

--- Practica/Assignment 1/code/assignment_a1.m
+++ Practica/Assignment 1/code/assignment_a1.m
@@ -1,7 +1,8 @@
 clc; clear all; close all force;
 
 K = 1;
-n = 10000;
+% n = 10000;
+n = 100;
 
 X_IDX = 1; P_IDX = 2;
 
@@ -35,47 +36,47 @@ for idx = 1 : length(x_0)
     xlabel('{x_n}');
     ylabel('{p_n}');
     
-    high_quality_plot('Save', sprintf('../report/img/assignment_a_%d_dim.pdf', idx - 1), 'FontSize', 22, 'PaperWidth', 8, 'PaperHeight', 8, 'Margin', 0, 'Box', 'on');
+    high_quality_plot('Save', sprintf('../report/img/assignment_a_%d_dim_n100.pdf', idx - 1), 'FontSize', 22, 'PaperWidth', 8, 'PaperHeight', 8, 'Margin', 0, 'Box', 'on');
     
     %% Plot progression of x
-    figure()
-    plot(map(1:number_of_plot_iterations - 1, X_IDX), map(2:number_of_plot_iterations, X_IDX), 'Color', colors(2,:));
-    xlim([-0.1, 1.1]);
-    ylim([-0.1, 1.1]);
-    
-    xlabel('{x_n}');
-    ylabel('{x_{n+1}}');
-    
-    high_quality_plot('Save', sprintf('../report/img/assignment_a_%d_dim_progression_x.pdf', idx - 1), 'FontSize', 22, 'PaperWidth', 8, 'PaperHeight', 8, 'Margin', 0, 'Box', 'on');
+%     figure()
+%     plot(map(1:number_of_plot_iterations - 1, X_IDX), map(2:number_of_plot_iterations, X_IDX), 'Color', colors(2,:));
+%     xlim([-0.1, 1.1]);
+%     ylim([-0.1, 1.1]);
+%     
+%     xlabel('{x_n}');
+%     ylabel('{x_{n+1}}');
+%     
+%     high_quality_plot('Save', sprintf('../report/img/assignment_a_%d_dim_progression_x.pdf', idx - 1), 'FontSize', 22, 'PaperWidth', 8, 'PaperHeight', 8, 'Margin', 0, 'Box', 'on');
     
     %% Plot progression of p
-    figure()
-    if(idx == 1)
-        scatter(map(1:number_of_plot_iterations - 1, P_IDX), map(2:number_of_plot_iterations, P_IDX), 100, 'Filled', 'MarkerFaceColor', colors(3,:));
-    else
-        plot(map(1:number_of_plot_iterations - 1, P_IDX), map(2:number_of_plot_iterations, P_IDX), 'Color', colors(3,:));
-    end
-    
-    xlim([-0.1, 1.1]);
-    ylim([-0.1, 1.1]);
-    
-    xlabel('{p_n}');
-    ylabel('{p_{n+1}}');
-    
-    high_quality_plot('Save', sprintf('../report/img/assignment_a_%d_dim_progression_p.pdf', idx - 1), 'FontSize', 22, 'PaperWidth', 8, 'PaperHeight', 8, 'Margin', 0, 'Box', 'on');
+%     figure()
+%     if(idx == 1)
+%         scatter(map(1:number_of_plot_iterations - 1, P_IDX), map(2:number_of_plot_iterations, P_IDX), 100, 'Filled', 'MarkerFaceColor', colors(3,:));
+%     else
+%         plot(map(1:number_of_plot_iterations - 1, P_IDX), map(2:number_of_plot_iterations, P_IDX), 'Color', colors(3,:));
+%     end
+%     
+%     xlim([-0.1, 1.1]);
+%     ylim([-0.1, 1.1]);
+%     
+%     xlabel('{p_n}');
+%     ylabel('{p_{n+1}}');
+%     
+%     high_quality_plot('Save', sprintf('../report/img/assignment_a_%d_dim_progression_p.pdf', idx - 1), 'FontSize', 22, 'PaperWidth', 8, 'PaperHeight', 8, 'Margin', 0, 'Box', 'on');
     
     %% Plot P as function of t
-    figure()
-    hold on
-    plot(1:number_of_t_plot_iterations, map(1:number_of_t_plot_iterations, P_IDX), '-', 'Color', colors(3, :), 'LineWidth', 3, 'Marker', '.', 'MarkerSize', 30, 'MarkerFaceColor', colors(3, :));
-    plot(1:number_of_t_plot_iterations, map(1:number_of_t_plot_iterations, X_IDX), '-.', 'Color', colors(2, :), 'LineWidth', 3, 'Marker', '.', 'MarkerSize', 30, 'MarkerFaceColor', colors(2, :));
-    hold off
-    xlabel('n')
-    ylabel('{x_n, p_n}')
-    
-    ylim([-0.1, 1.1]);
-    
-    high_quality_plot('Save', sprintf('../report/img/assignment_a_%d_dim_px_of_n.pdf', idx - 1), 'FontSize', 22, 'PaperWidth', 8, 'PaperHeight', 8, 'Margin', 0, 'Box', 'on');
+%     figure()
+%     hold on
+%     plot(1:number_of_t_plot_iterations, map(1:number_of_t_plot_iterations, P_IDX), '-', 'Color', colors(3, :), 'LineWidth', 3, 'Marker', '.', 'MarkerSize', 30, 'MarkerFaceColor', colors(3, :));
+%     plot(1:number_of_t_plot_iterations, map(1:number_of_t_plot_iterations, X_IDX), '-.', 'Color', colors(2, :), 'LineWidth', 3, 'Marker', '.', 'MarkerSize', 30, 'MarkerFaceColor', colors(2, :));
+%     hold off
+%     xlabel('n')
+%     ylabel('{x_n, p_n}')
+%     
+%     ylim([-0.1, 1.1]);
+%     
+%     high_quality_plot('Save', sprintf('../report/img/assignment_a_%d_dim_px_of_n.pdf', idx - 1), 'FontSize', 22, 'PaperWidth', 8, 'PaperHeight', 8, 'Margin', 0, 'Box', 'on');
     
     %% Plot period doubling x
     %     if(idx ~= 1)
