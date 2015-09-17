b745f91d864ae429063b79634a48be95b0ee8998
Laura Baakman <laurabaakman@gmail.com>
2015-09-17 12:33:16 +0200

    Extra plaatjes.

--- Practica/Assignment 1/code/assignment_a1.m
+++ Practica/Assignment 1/code/assignment_a1.m
@@ -16,6 +16,7 @@ colors = [
 
 
 number_of_plot_iterations = 500;
+number_of_t_plot_iterations = 30;
 
 lbmap(2, 'RedBlue');
 
@@ -26,55 +27,68 @@ for idx = 1 : length(x_0)
     map = chirikov_map(K, x_0(idx), p_0(idx), n);
     
     %% Plot p vs x
-        figure()
-        scatter(map(:, X_IDX), map(:, P_IDX), sizes(idx), 'filled', 'MarkerFaceColor', colors(1,:));
-        xlim([-0.1, 1.1]);
-        ylim([-0.1, 1.1]);
+    figure()
+    scatter(map(:, X_IDX), map(:, P_IDX), sizes(idx), 'filled', 'MarkerFaceColor', colors(1,:));
+    xlim([-0.1, 1.1]);
+    ylim([-0.1, 1.1]);
     
-        xlabel('{x_n}');
-        ylabel('{p_n}');
+    xlabel('{x_n}');
+    ylabel('{p_n}');
     
-        high_quality_plot('Save', sprintf('../report/img/assignment_a_%d_dim.pdf', idx - 1), 'FontSize', 22, 'PaperWidth', 8, 'PaperHeight', 8, 'Margin', 0.05, 'Box', 'on');
+    high_quality_plot('Save', sprintf('../report/img/assignment_a_%d_dim.pdf', idx - 1), 'FontSize', 22, 'PaperWidth', 8, 'PaperHeight', 8, 'Margin', 0, 'Box', 'on');
     
     %% Plot progression of x
-        figure()
-        plot(map(1:number_of_plot_iterations - 1, X_IDX), map(2:number_of_plot_iterations, X_IDX), 'Color', colors(2,:));
-        xlim([-0.1, 1.1]);
-        ylim([-0.1, 1.1]);
+    figure()
+    plot(map(1:number_of_plot_iterations - 1, X_IDX), map(2:number_of_plot_iterations, X_IDX), 'Color', colors(2,:));
+    xlim([-0.1, 1.1]);
+    ylim([-0.1, 1.1]);
     
-        xlabel('{x_n}');
-        ylabel('{x_{n+1}}');
+    xlabel('{x_n}');
+    ylabel('{x_{n+1}}');
     
-        high_quality_plot('Save', sprintf('../report/img/assignment_a_%d_dim_progression_x.pdf', idx - 1), 'FontSize', 22, 'PaperWidth', 8, 'PaperHeight', 8, 'Margin', 0.05, 'Box', 'on');
+    high_quality_plot('Save', sprintf('../report/img/assignment_a_%d_dim_progression_x.pdf', idx - 1), 'FontSize', 22, 'PaperWidth', 8, 'PaperHeight', 8, 'Margin', 0, 'Box', 'on');
     
     %% Plot progression of p
-        figure()
-        if(idx == 1)
-            scatter(map(1:number_of_plot_iterations - 1, P_IDX), map(2:number_of_plot_iterations, P_IDX), 100, 'Filled', 'MarkerFaceColor', colors(3,:));
-        else
-            plot(map(1:number_of_plot_iterations - 1, P_IDX), map(2:number_of_plot_iterations, P_IDX), 'Color', colors(3,:));
-        end
+    figure()
+    if(idx == 1)
+        scatter(map(1:number_of_plot_iterations - 1, P_IDX), map(2:number_of_plot_iterations, P_IDX), 100, 'Filled', 'MarkerFaceColor', colors(3,:));
+    else
+        plot(map(1:number_of_plot_iterations - 1, P_IDX), map(2:number_of_plot_iterations, P_IDX), 'Color', colors(3,:));
+    end
     
-        xlim([-0.1, 1.1]);
-        ylim([-0.1, 1.1]);
+    xlim([-0.1, 1.1]);
+    ylim([-0.1, 1.1]);
     
-        xlabel('{p_n}');
-        ylabel('{p_{n+1}}');
+    xlabel('{p_n}');
+    ylabel('{p_{n+1}}');
     
-        high_quality_plot('Save', sprintf('../report/img/assignment_a_%d_dim_progression_p.pdf', idx - 1), 'FontSize', 22, 'PaperWidth', 8, 'PaperHeight', 8, 'Margin', 0.05, 'Box', 'on');
+    high_quality_plot('Save', sprintf('../report/img/assignment_a_%d_dim_progression_p.pdf', idx - 1), 'FontSize', 22, 'PaperWidth', 8, 'PaperHeight', 8, 'Margin', 0, 'Box', 'on');
+    
+    %% Plot P as function of t
+    figure()
+    hold on
+    plot(1:number_of_t_plot_iterations, map(1:number_of_t_plot_iterations, P_IDX), '-', 'Color', colors(2, :), 'LineWidth', 3, 'Marker', '.', 'MarkerSize', 30, 'MarkerFaceColor', colors(3, :));
+    plot(1:number_of_t_plot_iterations, map(1:number_of_t_plot_iterations, X_IDX), '-.', 'Color', colors(3, :), 'LineWidth', 3, 'Marker', '.', 'MarkerSize', 30, 'MarkerFaceColor', colors(2, :));
+    hold off
+    xlabel('n')
+    ylabel('{x_n, p_n}')
+    
+    ylim([-0.1, 1.1]);
+    
+    high_quality_plot('Save', sprintf('../report/img/assignment_a_%d_dim_px_of_n.pdf', idx - 1), 'FontSize', 22, 'PaperWidth', 8, 'PaperHeight', 8, 'Margin', 0, 'Box', 'on');
     
     %% Plot period doubling x
-%     if(idx ~= 1)
-%         figure()
-%         hist(map(:, X_IDX), 1250);
-%         xlabel('{x_n}')
-%         ylabel('frequency')
-%         
-%         %% Plot period doubling x
-%         figure()
-%         hist(map(:, P_IDX), 1250);
-%         xlabel('{p_n}')
-%         ylabel('frequency')        
-    end
+    %     if(idx ~= 1)
+    %         figure()
+    %         hist(map(:, X_IDX), 1250);
+    %         xlabel('{x_n}')
+    %         ylabel('frequency')
+    %
+    %         %% Plot period doubling x
+    %         figure()
+    %         hist(map(:, P_IDX), 1250);
+    %         xlabel('{p_n}')
+    %         ylabel('frequency')
+    % end
     
 end
