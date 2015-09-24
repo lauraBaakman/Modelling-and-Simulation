8757a741d4b64f7b382467b0b31b09c446cd69bd
Laura Baakman <laurabaakman@gmail.com>
2015-09-24 13:50:10 +0200

    K = 0 verhaaltje met code voor plaatjes.

--- Practica/Assignment 1/code/assignment_bK0.m
+++ Practica/Assignment 1/code/assignment_bK0.m
@@ -1,12 +1,38 @@
 clc; clear all; close all force;
 
-p_0 = 0.9999999;
-% x_0s = 0:0.1:1;
-x_0s = 0.000001;
+%% Vary x_0
 
-hold on
+p_0 = 0.4;
+x_0s = 0.1:0.2:1;
+    
+colors = lbmap(1);
+
+
+idx = 1;
 for x_0 = x_0s
-    map = chirikov_map(0, x_0, p_0, 9999999999);
-    plot(1:length(map(:,1)), map(:,1));
+    map = chirikov_map(0, x_0, p_0, 20);
+    plot(1:length(map(:,1)), map(:,1), 'lineWidth', 2, 'color', colors);
+    idx = idx + 1;
+    xlabel('n');
+    ylabel('{x_n}')
+    high_quality_plot('Save', sprintf('../report/img/assignment_b_K=0p_0=04x_0=0%d.pdf', round(x_0*10)), 'FontSize', 22, 'PaperWidth', 8, 'PaperHeight', 2, 'Margin', 0, 'Box', 'on');
 end
-hold off
+
+
+%% Vary p_0
+
+p_0s = 0.1:0.2:1;
+x_0 = 0.4;
+    
+colors = lbmap(1);
+
+
+idx = 1;
+for p_0 = p_0s
+    map = chirikov_map(0, x_0, p_0, 20);
+    plot(1:length(map(:,1)), map(:,1), 'lineWidth', 2, 'color', colors);
+    idx = idx + 1;
+    xlabel('n');
+    ylabel('{x_n}')
+    high_quality_plot('Save', sprintf('../report/img/assignment_b_K=0p_0=0%dx_0=04.pdf', round(p_0*10)), 'FontSize', 22, 'PaperWidth', 8, 'PaperHeight', 2, 'Margin', 0, 'Box', 'on');
+end
--- Practica/Assignment 1/code/high_quality_plot.m
+++ Practica/Assignment 1/code/high_quality_plot.m
@@ -60,6 +60,6 @@ function high_quality_plot( varargin )
     set(gcf, 'PaperPositionMode', 'Manual');
     
     if ~strcmp(p.Results.Save, '')
-        print(gcf, '-painters', '-dpdf', '-r600', p.Results.Save);
+        print(gcf, '-painters', '-dpdf', '-r300', p.Results.Save);
     end
 end
