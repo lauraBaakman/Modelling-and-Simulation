85c4ad76b741f4ab5537a44c304bc2e570eec5a8
Rick van Veen <r.van.veen133@gmail.com>
2015-09-24 14:09:21 +0200

    Fancy images for different k

--- Practica/Assignment 1/code/assignment_bK0.m
+++ Practica/Assignment 1/code/assignment_bK0.m
@@ -1,12 +1,12 @@
 clc; clear all; close all force;
 
-p_0 = 0.9999999;
+p_0 = 0.8;
 % x_0s = 0:0.1:1;
-x_0s = 0.000001;
+x_0s = 0.2;
 
 hold on
 for x_0 = x_0s
-    map = chirikov_map(0, x_0, p_0, 9999999999);
+    map = chirikov_map(0, x_0, p_0, 10);
     plot(1:length(map(:,1)), map(:,1));
 end
 hold off
