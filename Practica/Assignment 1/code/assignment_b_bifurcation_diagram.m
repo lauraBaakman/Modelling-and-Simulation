daa23f7f360771920b93c65dbb895586bfadfe35
Rick van Veen <r.van.veen133@gmail.com>
2015-09-26 15:43:31 +0200

    Things and HQ plot options

--- Practica/Assignment 1/code/assignment_b_bifurcation_diagram.m
+++ Practica/Assignment 1/code/assignment_b_bifurcation_diagram.m
@@ -29,14 +29,14 @@ for init_idx = 1:length(x_0s)
         scatter(reshape(repmat(Ks, n, 1), [], 1), reshape(squeeze(maps(:, X_IDX, :)), [], 1), marker.size, marker.colour, marker.type);
         xlabel('K')
         ylabel('{x_n}')
-        title(sprintf('x_0 = %2.3f, p_0 = %2.3f', x_0, p_0));
+%         title(sprintf('x_0 = %2.3f, p_0 = %2.3f', x_0, p_0));
         high_quality_plot('Save', sprintf('../report/img/assignment_b_dim_%d_b_x.pdf', init_idx -1), 'FontSize', 22, 'PaperWidth', 8, 'PaperHeight', 8, 'Margin', 0, 'Box', 'on');
     
         figure('Visible','off')
         scatter(reshape(repmat(Ks, n, 1), [], 1), reshape(squeeze(maps(:, P_IDX, :)), [], 1), marker.size, marker.colour, marker.type);
         xlabel('K')
         ylabel('{p_n}')
-        title(sprintf('x_0 = %2.3f, p_0 = %2.3f', x_0, p_0));        
+%         title(sprintf('x_0 = %2.3f, p_0 = %2.3f', x_0, p_0));        
         high_quality_plot('Save', sprintf('../report/img/assignment_b_dim_%d_b_p.pdf', init_idx -1), 'FontSize', 22, 'PaperWidth', 8, 'PaperHeight', 8, 'Margin', 0, 'Box', 'on');
         
 end
