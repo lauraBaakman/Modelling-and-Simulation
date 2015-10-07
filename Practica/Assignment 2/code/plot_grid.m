d78d72b72171bd12f2f81167005645226b8764c1
Rick van Veen <r.van.veen133@gmail.com>
2015-10-07 16:42:46 +0200

    Dingen

--- Practica/Assignment 2/code/plot_grid.m
+++ Practica/Assignment 2/code/plot_grid.m
@@ -23,7 +23,7 @@ function [] = plot_grid(queue, grid, mask, pixelWidth, filename)
     imagesc(colour_grid);
     axis off;
     set(gca, 'position', [0 0 1 1]);
-    high_quality_plot('Save', sprintf('../report/img/map_%d_%d', size(grid, 1), size(grid, 2)), 'Ext', 'jpeg', 'Dpi', 300, ...
+    high_quality_plot('Save', sprintf('../report/img/%s', filename), 'Ext', 'jpeg', 'Dpi', 300, ...
     'FontSize', 10, 'PaperSize', 442.65375, 'PaperWidthRatio', 0.24, 'PaperWidthHeightRatio', 1, 'Margin', 0);
 %     imshow(colour_grid, 'InitialMagnification', 'fit');
 %     imwrite(colour_grid, filename);
