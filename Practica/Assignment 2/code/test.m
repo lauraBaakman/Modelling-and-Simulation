485584ec6280063b588216294113f0ad59253dc2
Rick van Veen <r.van.veen133@gmail.com>
2015-10-11 10:20:17 +0200

    .

--- Practica/Assignment 2/code/test.m
+++ Practica/Assignment 2/code/test.m
@@ -4,11 +4,15 @@ global stop_reasons
 stop_reasons.PERCOLATING = 0;
 stop_reasons.FINITE = 1;
     
-rng(3);
+% rng(3);
 
-N = 5;
-ps = (0.3:0.01:0.7);
-max_runs = 200;
+% N = 5;
+% ps = (0.3:0.01:0.7);
+% max_runs = 200;
+
+N = 1000;
+% ps = 0.6;
+% max_runs = 200;
 
 mask = ones(3,3);
 mask(1,1) = 0;
@@ -16,6 +20,8 @@ mask(1,3) = 0;
 mask(3,1) = 0;
 mask(3,3) = 0;
 
-[grid, queue, stop_condition] = percolation(N, mask, 0.3);
+tic
+[grid, queue, stop_condition] = percolation(N, mask, 0.6);
+toc
 
 plot_grid(queue, grid, mask, size(grid, 2), 'test.png');
