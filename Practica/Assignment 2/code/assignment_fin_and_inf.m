9ee2c243fd16e99e6497133ad39e90565782722d
Laura Baakman <laurabaakman@gmail.com>
2015-10-11 13:41:29 +0200

    Speciefke clusters.

--- Practica/Assignment 2/code/assignment_fin_and_inf.m
+++ Practica/Assignment 2/code/assignment_fin_and_inf.m
@@ -5,7 +5,8 @@ stop_reasons.PERCOLATING = 0;
 stop_reasons.FINITE = 1;
 
 Ns = [20];
-ps = [0.56, 0.71];
+ps = [0.3, 0.6, 0.5];
+seeds = [8, 5, 8];
 
 mask = ones(3,3);
 mask(1,1) = 0;
@@ -13,11 +14,17 @@ mask(1,3) = 0;
 mask(3,1) = 0;
 mask(3,3) = 0;
 
+idx = 1;
+
+data = nan(size(ps, 1) * 20, 3);
+
 for N = Ns
-    for p = ps
-        rng(314159);
-        [grid, queue, stop_condition] = percolation(N, mask, p);
-        plot_grid(queue, grid, mask, size(grid, 2), sprintf('fancy_cluster_N%d_p%d.jpg', N, 10*p));
+    for i = 1:length(ps)
+        p = ps(i);
+        seed = seeds(i);
+            rng(seed);
+            [grid, queue, ~] = percolation(N, mask, p);
+            plot_grid(queue, grid, mask, size(grid, 2), sprintf('fancy_cluster_N%d_p%d_rng_%d.jpg', N, 10*p, seed));
     end
 end
 
