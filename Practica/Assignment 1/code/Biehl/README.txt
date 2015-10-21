09f0e615f0446531e550375e4e9ed90e8f88c9ea
Laura Baakman <laurabaakman@gmail.com>
2015-10-21 15:29:57 +0200

    .

--- Practica/Assignment 1/code/Biehl/README.txt
+++
@@ -1,4 +0,0 @@
-
- At the Matlab prompt, type intro to 
- obtain a list of available commands
-
--- Practica/Assignment 1/code/Biehl/explain.m
+++
@@ -1,7 +0,0 @@
-
-% logistic map
-% the iteration  x(i+1) = 4 r x(i) (1- x(i))
-% is implemented and can be studied by the
-% following scripts and functions:
-%
-
